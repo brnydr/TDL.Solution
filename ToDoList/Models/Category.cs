ce5d9854fcf61ef0a66954154bd4f31fbf701d59
brnydr <brianwilliamyoder@gmail.com>
2023-10-22 11:29:28 -0700

    add error message

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Category 
   {
+    
     public int CategoryId { get; set; }
+    [Required(ErrorMessage = "Categories must have a name.")]
     public string Name { get; set; }
     public List<Item> Items { get; set; }
   }
