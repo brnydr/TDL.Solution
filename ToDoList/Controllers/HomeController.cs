13e2ab2a7343d725180e49bc1d0c065c17a39098
brnydr <brianwilliamyoder@gmail.com>
2023-10-22 11:28:34 -0700

    refactor for auth

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,15 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using ToDoList.Models;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, ToDoListContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
 
       [HttpGet("/")]
-      public ActionResult Index()
+      public async Task<ActionResult> Index()
       {
-        ViewBag.PageTitle = "Welcome to the To Do List!";
-        return View();
+        Category[] cats = _db.Categories.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("categories", cats);
+        string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        if (currentUser != null)
+        {
+          Item[] items = _db.Items
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("items", items);
+        }
+        return View(model);
       }
 
     }
